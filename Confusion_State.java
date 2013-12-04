3d980d26945d06a3959045d6084342d59d8f68fd
kevinnmnliu <kl4bx@virginia.edu>
2013-12-04 17:05:01 -0500

    Newly Added States, as well as a Decorator Design Pattern to randomize the final states even more

---
+++ Confusion_State.java
@@ -0,0 +1,17 @@
+ package devforrest.mario.objects.mario;
+
+public class Confusion_State implements Mario_State {
+
+	public void setSpace(Mario m, boolean set){
+		m.setSpace(set);
+	}
+	public void setLeft(Mario m, boolean set){
+		m.setDown(set);
+	}
+	public void setRight(Mario m, boolean set){
+		m.setLeft(set);
+	}
+	public void setDown(Mario m, boolean set){
+		m.setRight(set);
+	}
+}
